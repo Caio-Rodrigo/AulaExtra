7f8b323405f94c900792cd39d28d936fcc505863
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-07-01 20:13:58 -0300

    chore:init

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const express = require('express');
+const express = require('express'); /* importando o express */
+const cors = require('cors');
 
+const port = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); /* ativando o express */
+
+app.listen(port, () => {
+   console.log(`Server listening on port http://localhost:${port}`);
+});
